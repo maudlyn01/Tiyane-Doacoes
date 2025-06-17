759858c892c947109496632740a2cca3c21917d1
Cris da Silva <jnc.mataca@gmail.com>
2025-06-17 17:06:20 +0200

    feat:update

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,3 +1,9 @@
 export const Header = () => {
-  return <div className="bg-red-500 text-white">Header</div>;
+  return <div className="bg-gradient-to-r from-gray-900 via-purple-900 to-purple-800 text-white text-xl flex justify-between items-center h-15">
+    <p>B4F</p>
+    <div><a className="px-5" href="http://">Admin</a>
+    <a href="http://">Intermediario</a>
+  
+    </div>
+  </div>;
 };
