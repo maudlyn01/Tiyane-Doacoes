35ec02898001188df595273fabb6933542439e55
Cris da Silva <jnc.mataca@gmail.com>
2025-06-17 17:57:08 +0200

    Feat:add

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,3 +1,10 @@
 export const Home = () => {
-  return <div className="min-h-screen flex flex-col bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 text-gray-100 dark:from-slate-900 dark:via-purple-900 dark:to-slate-900 dark:text-gray-100 selection:bg-teal-500 selection:text-white transition-colors duration-500">home page</div>;
+  return <div className="min-h-screen flex items-center flex-col bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 text-gray-100 dark:from-slate-900 dark:via-purple-900 dark:to-slate-900 dark:text-gray-100 selection:bg-teal-500 selection:text-white transition-colors duration-500">
+    <div ><h1 className="text-5xl font-extrabold bg-gradient-to-r from-cyan-400 via-blue-600 to-pink-400 text-transparent bg-clip-text">Tiyane Doação</h1>
+    </div>
+     <p>é uma instituição de caridade sem fins lucrativos fundada em 2025 com o propósito de transformar a solidariedade em ação concreta. Surgimos a partir da necessidade de ajudar famílias vulneráveis em Moçambique, promovendo a redistribuição justa de bens e o fortalecimento da dignidade humana.
+</p>
+   
+    
+    </div>;
 };
