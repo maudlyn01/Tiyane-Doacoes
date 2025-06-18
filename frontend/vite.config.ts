d1d37ffa0eba73bd3be43d7bef5ae2719177ad64
Moises2304 <ngovenemoises2@gmail.com>
2025-06-18 11:43:22 +0200

    resolve conflict

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,17 @@
-import * as path from "path";
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import * as path from "path";
+// import tailwindcss from "@tailwindcss/vite"
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server:{
-    port:5001,
-  },
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server:{
+//     port:5001,
+//   },
+// })
